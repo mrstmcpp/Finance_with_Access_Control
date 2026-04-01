b8a37e30777eb3dc9fcbb0b9979bae795edffa4e
Satyam Prajapati <mrstm.cpp@gmail.com>
2026-04-01 21:55:39 +0530

    Initial setup

---
+++ src/main/java/org/mrstm/zorvynfinance/ZorvynFinanceApplication.java
@@ -0,0 +1,13 @@
+package org.mrstm.zorvynfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZorvynFinanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZorvynFinanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mrstm/zorvynfinance/ZorvynFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mrstm.zorvynfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZorvynFinanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
